275669394c5959b40529bac6a6ad2b642e6ea7f8
engbilalali <158054635+engbilalali@users.noreply.github.com>
2024-10-09 17:26:42 +0500

    Update Fetch_Unit.v

--- RV32I_Processor/Processor_part2.srcs/sources_1/new/Fetch_Unit.v
+++ RV32I_Processor/Processor_part2.srcs/sources_1/new/Fetch_Unit.v
@@ -8,10 +8,34 @@ output wire [31:0] PC,
 output wire [31:0] Instruction
     );
     
+    reg [31:0] PC_next;
+    
     Instruction_Memory memory(.addr(PC),.rd(Instruction));
     
     
-    Program_Counter counter(.clk(clk),.reset(reset),.ExtImmediate(ExtImmediate),.ALU_result(ALU_result),.PC_sel(PC_sel),.PC(PC));
+    Program_Counter counter(.clk(clk),.reset(reset),.PC_next(PC_next),.PC(PC));
+    
+    
+    always @(*) begin
+        case (PC_sel)
+            2'b00:  
+            begin
+               PC_next = PC + 4; 
+            end
+            2'b01: 
+            begin
+               PC_next = ALU_result;
+            end
+            2'b10: 
+            begin
+               PC_next = PC + ExtImmediate; 
+            end
+            2'b11:  
+            begin
+               PC_next = 32'hXXXXXXXX;
+            end
+        endcase
+    end    
     
     
     
